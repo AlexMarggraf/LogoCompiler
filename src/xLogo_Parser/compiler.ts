178a2da4d544c7a9f01b27ef751fd66645e848f9
Pascal von Fellenberg <pvonfellenberg@bluewin.ch>
2025-12-25 22:09:30 +0100

    update compiler to be compatible with client code or something

--- src/xLogo_Parser/compiler.ts
+++ src/xLogo_Parser/compiler.ts
@@ -20,18 +20,24 @@ import {
 import {ASTVisitor} from './ASTVisitor.js';
 
 import assert from "assert";
-import { AssignmentExpression, BlockStatement, CallExpression, ExpressionStatement, FunctionDeclaration, Identifier, Literal, Script, StaticMemberExpression, VariableDeclaration, VariableDeclarator } from "./esnodes.js";
+import { ArrowFunctionExpression, AssignmentExpression, AsyncArrowFunctionExpression, AsyncFunctionDeclaration, AwaitExpression, BinaryExpression, BlockStatement, CallExpression, ExpressionStatement, FunctionDeclaration, Identifier, Literal, Script, StaticMemberExpression, UnaryExpression, VariableDeclaration, VariableDeclarator } from "./esnodes.js";
 import { Program, BaseNode } from "estree";
 
 function compileCodeToAST(logocode: string): Program {
-  const ast = parseCode(logocode);
+  const ast = parseCode(logocode.toLowerCase());
 
+  //ast.accept(new DebugVisitor(), 0)
   const esast = ast.accept(new CompilerVisitor(), 0);
-  console.log(JSON.stringify(esast, null, 2));
+  //console.log(JSON.stringify(esast, null, 2));
   return esast;
 }
-export function compileCode(logocode: string): string{
-  return generate(compileCodeToAST(logocode));
+export function compileCode(logocode: string): string {
+  var code = generate(compileCodeToAST(logocode));
+  code = `
+  const pi = 3.14159265358979323846264338327950288419716939937510582097;
+  const e = 2.718281828459045235360287471352662497757247093699959574966;
+  ` + code;
+  return code;
 }
 
 function isBody(body: any): body is Array<BaseNode> {
@@ -49,18 +55,24 @@ function varNameMangle(name: string): string {
 }
 
 function funcNameMangle(name: string): string {
-  return "_XLogoCode_func_" + name;
+  return "" + name;
+}
+
+function generateActionSetCall(callname: string, args: any[]): BaseNode {
+  const res = new CallExpression(new StaticMemberExpression(new Identifier("act"), new Identifier(callname)), args);
+  if (callname === "wait") {
+    return new AwaitExpression(res);
+  }
+  return res;
 }
 
 export class CompilerVisitor extends ASTVisitor<number, any> {
   public defaultNode(ast: XLogoAST, args: number): BaseNode {
     throw new Error("not implemented yet!")
-    console.log('|'.repeat(args) + ast.constructor.name);
-    this.visitChildren(ast, args + 1);
   }
 
   public aggregateResult(_aggregate: any, nextResult: any) {
-    console.log("in aggregateResult", _aggregate)
+    //console.log("in aggregateResult", _aggregate)
     if (!Array.isArray(_aggregate)) throw new Error("Aggregate must be of type array");
     _aggregate.push(nextResult);
     return _aggregate;
@@ -73,25 +85,38 @@ export class CompilerVisitor extends ASTVisitor<number, any> {
 
     if (args == 0) {
       console.log("visiting top level seq");
-      return new Script(body) as Program;
+      return new Script([new CallExpression(new AsyncArrowFunctionExpression([], new BlockStatement(body), true), [])]) as Program;
     } else {
       // assumption: we are in progdeclaration
       console.log("visiting seq, not at top level");
-      return body;
+      // we have to convert expressions to statements
+      return body.map((exp) => {
+        if (exp.type == "VariableDeclaration") {
+          return exp;
+        }
+        return new ExpressionStatement(exp)
+      });
     }
+  }
+  public visitBuiltInCommand(ast: BuiltInCommand, args: number) {
+    //console.log("in visitBuiltinCommand\n", ast)
+    const callArgs = this.visitChildren(ast, args + 1);
+    return generateActionSetCall(ast.commandName, callArgs);
   }
 
   public visitProgDecl(ast: ProgDecl, args: number): BaseNode {
-    const statements = this.visitChildren(ast, args + 1)[0]; // TODO figure out why statements is an array of array of object
+    const statements = this.visitChildren(ast, args + 1)[0]; // TODO figure out why visitChildren gives back an array of array of object
+    if (ast.name === "main") { // insert entrypoint into script. in code this would look like: (() => {...})()
+      return new CallExpression(new ArrowFunctionExpression([], new BlockStatement(statements), false), []);
+    }
     const funcname = funcNameMangle(ast.name);
-    const id = new Identifier(funcname);
     const params = ast.args.map((ast) => {return new Identifier(ast.name.slice(1));})
-    return new FunctionDeclaration(new Identifier(funcname), params, new BlockStatement(statements), false);
+    return new AsyncFunctionDeclaration(new Identifier(funcname), params, new BlockStatement(statements));
   }
 
   public visitMakeStmt(ast: MakeStmt, args: number): BaseNode {
     // TODO how do we handle assignments before declaration? the implementation of xlogoonline doesn't care. 
-    console.log(ast);
+    //console.log(ast);
     assert(ast.declName instanceof VarDecl);
 
     const varname = ast.declName.name.slice(1)
@@ -102,9 +127,48 @@ export class CompilerVisitor extends ASTVisitor<number, any> {
     if (ast.declName.name.startsWith("\"")) {
       return new VariableDeclaration([new VariableDeclarator(id, assigner)], "let")
     } else if (ast.declName.name.startsWith(":")) {
-      return new ExpressionStatement(new AssignmentExpression("=", id, assigner))
+      return new AssignmentExpression("=", id, assigner)
     } else {
       throw new Error("should not be reached");
+    }
+  }
+
+  public visitFuncExpr(ast: FuncExpr, args: number): BaseNode {
+    const actionsetfuncs = ["random", "mod", "power", "sqrt", "log", "abs", "sin", "cos", "tan", "arcsin", "arccos", "arctan"]
+    const consts = ["pi", "e"];
+    if (actionsetfuncs.includes(ast.name)) {
+      const callArgs = this.visitChildren(ast, args + 1);
+      return generateActionSetCall(ast.name, callArgs);
+    } else if (consts.includes(ast.name)) {
+      return new Identifier(ast.name);
+    } else {
+      throw new Error("should not be reached!")
+    }
+  }
+
+  public visitProgCallStmt(ast: ProgCallStmt, args: number) {
+    const callArgs = this.visitChildren(ast, args + 1);
+    return new CallExpression(new Identifier(ast.progName), callArgs);
+  }
+
+  public visitUnaryOpExpr(ast: UnaryOpExpr, args: number): BaseNode {
+    const arg = this.visitChildren(ast, args + 1);
+    assert(arg.length = 1);
+    if (["-", "+"].includes(ast.operator)) {
+      return new UnaryExpression(ast.operator, arg[0]);
+    } else {
+      //console.log(ast.operator);
+      throw new Error("not implemented yet!");
+    }
+  }
+
+  public visitBinaryOpExpr(ast: BinaryOpExpr, args: number): BaseNode {
+    const opArgs = this.visitChildren(ast, args + 1);
+    assert(opArgs.length == 2);
+    if (["+", "-", "*", "/", "%"].includes(ast.operator)) {
+      return new BinaryExpression(ast.operator, opArgs[0], opArgs[1]);
+    } else {
+      throw new Error("not implemented yet!");
     }
   }
 
@@ -113,8 +177,15 @@ export class CompilerVisitor extends ASTVisitor<number, any> {
   }
 
   public visitPrintStmt(ast: PrintStmt, args: number): BaseNode {
-    const argsOfCall = this.visitChildren(ast, args + 1);
-    return new ExpressionStatement(new CallExpression(new StaticMemberExpression(new Identifier("console"), new Identifier("log")), argsOfCall));
+    let argsOfCall = this.visitChildren(ast, args + 1);
+    if (Array.isArray(argsOfCall) && argsOfCall.length == 0) {
+      //console.log(ast)
+      const input = ast.argString();
+      console.log(input);
+      assert(input)
+      argsOfCall = [new Literal(input, "\"" + input + "\"")]
+    }
+    return generateActionSetCall("print", argsOfCall);
   }
 
   public visitVarExpr(ast: VarExpr, args: number): BaseNode {
@@ -161,8 +232,8 @@ to main
   rt 90 fd 100 * arcsin 0.4
   rt 90 fd 100 * arccos 0.4
   rt 90 fd 100 * arctan 0.4
-  #rt 90 fd 100 * PI # TODO
-  #rt 90 fd 100 * E # TODO
+  rt 90 fd 100 * PI
+  rt 90 fd 100 * E
   print 67
   print [yoplait]
   ct
@@ -180,22 +251,58 @@ end
   `)*/
 
 import { diff, applyChangeset } from 'json-diff-ts';
-
-const compiled = compileCodeToAST(`
-to main :x
-  make "a 6
-  make :a 7
-  print :a
-end
-`);
+import { DebugVisitor } from "./debug/debugVisitor.js";
 const reference = parseModule(`
-function _XLogoCode_func_main(x) {
-  let a = 6;
-  a = 7;
-  console.log(a)
-}
+  funccall("string");
 `)
 console.log(JSON.stringify(reference, null, 2));
+
+const compiled = compileCodeToAST(`
+to coolfun :x
+  fd 10
+  lt 90 bk 20
+  rt 270 fd 30
+  #setpc red rt 90 fd 40
+  #setsc black
+  wash
+  #setpc [43 167 200] rt 90 fd 50
+  #setsc [23 20 67]
+  # ignore this twin ✌️
+  setpw 5 rt 90 fd 60 setpw 1
+  pu rt 90 fd 70 pd
+  rt 90 fd 80 bk 40 pe fd 40 ppt
+  cs
+  setx -70
+  sety 70
+  home
+  setxy 70 70
+  setheading 180
+  fd random 90
+  rt 90 fd mod 1000 300
+  rt 90 fd power 2 7
+  rt 90 fd sqrt 14400
+  rt 90 fd log 1000
+  rt 90 fd abs -140
+  rt 90 fd 100 * sin 0.4
+  rt 90 fd 100 * cos 0.4
+  rt 90 fd 100 * tan 0.4
+  rt 90 fd 100 * arcsin 0.4
+  rt 90 fd 100 * arccos 0.4
+  rt 90 fd 100 * arctan 0.4
+  rt 90 fd 100 * PI # TODO
+  rt 90 fd 100 * E # TODO
+  print 67
+  print [yoplait]
+  ct
+  make "a 6
+  make :a 67
+  wait 100
+  rt 90 fd 100
+  customfun 345
+end
+to customfun :x
+  print :x
+end`);
 
 //console.log(JSON.stringify(diff(compiled, reference), null, 2))
 console.log(generate(compiled))
