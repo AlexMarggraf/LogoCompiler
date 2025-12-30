b9b42227451792c12a7e9f17b3f87262352948fd
Alexander Marggraf <a.marggraf@stud.unibas.ch>
2025-12-30 20:51:57 +0100

    Adjust action set

--- src/ActionSet.ts
+++ src/ActionSet.ts
@@ -58,9 +58,6 @@ export class CanvasActionSet implements ActionSet{
   private screenColor: [number, number, number];
   private penDown: boolean;
 
-  static readonly PI: number = Math.PI;
-  static readonly E: number = Math.E;
-
   public constructor(ctx: CanvasRenderingContext2D) {
     this.ctx = ctx
     this.turtleX = ctx.canvas.width/2;
@@ -180,54 +177,6 @@ export class CanvasActionSet implements ActionSet{
   public setheading(angle: number) {
     this.turtleAngle = (270 + angle) % 360;
   }
-
-  /* public random(max: number): number {
-    return Math.random() * max;
-  }
-
-  public mod(a: number, b: number): number {
-    return a % b
-  }
-
-  public power(a: number, b: number): number {
-    return Math.pow(a, b);
-  }
-
-  public sqrt(a: number): number {
-    return Math.sqrt(a);
-  }
-
-  public log(a: number): number {
-    return Math.log10(a);
-  }
-  
-  public abs(a: number): number {
-    return Math.abs(a);
-  }
-
-  public sin(a: number): number {
-    return Math.sin(a);
-  }
-
-  public cos(a: number): number {
-    return Math.cos(a);
-  }
-
-  public tan(a: number): number {
-    return Math.tan(a);
-  }
-
-  public arcsin(a: number): number {
-    return Math.asin(a);
-  }
-
-  public arccos(a: number): number {
-    return Math.acos(a);
-  }
-
-  public arctan(a: number): number {
-    return Math.atan(a);
-  } */
 
   public print(content: number | string){
     console.log(content);
@@ -378,54 +327,6 @@ export class LogActionSet implements ActionSet{
     console.log(`setheading(${angle}), angle = ${this.turtleAngle}`);
   }
 
-  public random(max: number) {
-    console.log(`random(${max}), result = ${Math.random() * max}`);
-  }
-
-  public mod(a: number, b: number) {
-    console.log(`mod(${a}, ${b}), result = ${a % b}`);
-  }
-
-  public power(a: number, b: number) {
-    console.log(`power(${a}, ${b}), result = ${Math.pow(a, b)}`);
-  }
-
-  public sqrt(a: number) {
-    console.log(`sqrt(${a}), result = ${Math.sqrt(a)}`);
-  }
-
-  public log(a: number) {
-    console.log(`log(${a}), result = ${Math.log10(a)}`);
-  }
-  
-  public abs(a: number) {
-    console.log(`abs(${a}), result = ${Math.abs(a)}`);
-  }
-
-  public sin(a: number) {
-    console.log(`sin(${a}), result = ${Math.sin(a)}`);
-  }
-
-  public cos(a: number) {
-    console.log(`cos(${a}), result = ${Math.cos(a)}`);
-  }
-
-  public tan(a: number) {
-    console.log(`tan(${a}), result = ${Math.tan(a)}`);
-  }
-
-  public arcsin(a: number) {
-    console.log(`arcsin(${a}), result = ${Math.asin(a)}`);
-  }
-
-  public arccos(a: number) {
-    console.log(`arccos(${a}), result = ${Math.acos(a)}`);
-  }
-
-  public arctan(a: number) {
-    console.log(`arctan(${a}), result = ${Math.atan(a)}`);
-  }
-
   public print(content: number | string){
     console.log(`print(${content})`);
   }
@@ -441,8 +342,4 @@ export class LogActionSet implements ActionSet{
 
 function toRadians(degrees: number) {
     return degrees * Math.PI / 180
-}
-
-function toDegrees(radians: number) {
-    return radians * 180 / Math.PI
 }
